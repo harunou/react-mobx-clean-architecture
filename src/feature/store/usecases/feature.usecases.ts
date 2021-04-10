66e7bb823a215cd049e7fa73e338ad82986bbf8c
harunou <d.harunou@gmail.com>
2021-04-10 16:50:17 +0200

    Feature: add IncreaseValueAndSaveOptimistic use case

--- src/feature/store/usecases/feature.usecases.ts
+++ src/feature/store/usecases/feature.usecases.ts
@@ -2,6 +2,7 @@ import { DomainModel } from '../../../stores/domainStore/domainStore.types';
 import { InteractionBuilder } from '../../../stores/helpers/stores.helpers';
 import { UseCase } from '../../../stores/helpers/stores.types';
 import { PersistenceModel } from '../../../stores/persistenceStore/persistenceStore.types';
+import { SaveCountSuccessFlow } from '../flows/feature.flows';
 
 export class IncreaseValue implements UseCase {
     static make(store: DomainModel, params: number): IncreaseValue {
@@ -14,21 +15,26 @@ export class IncreaseValue implements UseCase {
         this.store.setCount(this.store.$count + this.params);
     }
 }
-
 export const IncreaseValueUseCase = InteractionBuilder.make(IncreaseValue);
 
-export class IncreaseValueAndOptimisticSave implements UseCase {
+export class IncreaseValueAndSaveOptimistic implements UseCase {
     static make(
         store: DomainModel,
         persistence: PersistenceModel,
         params: number
-    ): IncreaseValue {
-        return new IncreaseValue(store, params);
+    ): IncreaseValueAndSaveOptimistic {
+        const flow = SaveCountSuccessFlow.make(persistence);
+        return new IncreaseValueAndSaveOptimistic(store, flow, params);
     }
 
-    constructor(private store: DomainModel, private params: number) {}
+    constructor(
+        private store: DomainModel,
+        private flow: SaveCountSuccessFlow,
+        private params: number
+    ) {}
 
-    execute(): void {
+    async execute(): Promise<void> {
         this.store.setCount(this.store.$count + this.params);
+        await this.flow.save(this.store.$count);
     }
 }
