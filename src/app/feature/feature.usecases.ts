dce6f19f94c0a855386a3c6921315b2ea85adf26
harunou <d.harunou@gmail.com>
2021-04-10 14:57:12 +0200

    Feature: add increase use case

---
+++ src/app/feature/feature.usecases.ts
@@ -0,0 +1,17 @@
+import { DomainModel } from '../stores/domainStore/domainStore.types';
+import { InteractionBuilder } from '../stores/helpers/stores.helpers';
+import { UseCase } from '../stores/helpers/stores.types';
+
+export class IncreaseValue implements UseCase {
+    static make(store: DomainModel, params: number): IncreaseValue {
+        return new IncreaseValue(store, params);
+    }
+
+    constructor(private store: DomainModel, private params: number) {}
+
+    execute(): void {
+        this.store.setCount(this.store.$count + this.params);
+    }
+}
+
+export const IncreaseValueUseCase = InteractionBuilder.make(IncreaseValue);
