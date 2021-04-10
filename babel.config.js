f2fa3340e8b2f62c727be79864aaa7671eee9725
harunou <d.harunou@gmail.com>
2021-04-10 12:08:57 +0200

    Project: add mobx

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+const config = {
     presets: [
         ['@babel/preset-env', { targets: { node: 'current' } }],
         '@babel/preset-typescript'
+    ],
+    plugins: [
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
+        ['@babel/plugin-proposal-class-properties', { loose: false }]
     ]
 };
+
+module.exports = config;
