f1590d730b4bd7c4d31e84718c2ca390dc57013b
harunou <d.harunou@gmail.com>
2021-04-11 15:00:38 +0200

    Feature: fix omptimistic save test

--- src/feature/store/usecases/increase-value-and-save-optimistic.usecase.spec.ts
+++ src/feature/store/usecases/increase-value-and-save-optimistic.usecase.spec.ts
@@ -1,3 +1,4 @@
+import { CancellablePromise } from 'mobx/dist/internal';
 import { counterServiceMock } from '../../../api/counter.mocks';
 import { CounterStore } from '../../../stores/counter/counter.store';
 import { CounterModel } from '../../../stores/counter/counter.types';
@@ -12,10 +13,10 @@ describe(`${IncreaseValueAndSaveOptimistic.name}`, () => {
     beforeEach(() => {
         store = new CounterStore({ $count: 3 });
         effect = new SaveCountSuccessEffect(counterServiceMock);
-        jest.spyOn(effect, 'save');
     });
     it('increases model value on predefined amount and optimistically save to the BE', () => {
         const setCountSpy = jest.spyOn(store, 'setCount');
+        const saveSpy = jest.spyOn(effect, 'save');
         const useCase = new IncreaseValueAndSaveOptimistic(
             store,
             effect,
@@ -24,10 +25,15 @@ describe(`${IncreaseValueAndSaveOptimistic.name}`, () => {
         useCase.execute();
         expect(setCountSpy).toBeCalledTimes(1);
         expect(setCountSpy).toBeCalledWith(7);
-        expect(effect.save).toBeCalledWith(7);
+        expect(saveSpy).toBeCalledWith(7);
     });
     it('decreases model value on predefined amount and if save to the BE failed', async () => {
         const setCountSpy = jest.spyOn(store, 'setCount');
+        const saveSpy = jest
+            .spyOn(effect, 'save')
+            .mockImplementationOnce(
+                () => Promise.reject('Error') as CancellablePromise<number>
+            );
         const useCase = new IncreaseValueAndSaveOptimistic(
             store,
             effect,
@@ -36,7 +42,7 @@ describe(`${IncreaseValueAndSaveOptimistic.name}`, () => {
         useCase.execute();
         expect(setCountSpy).toBeCalledTimes(1);
         expect(setCountSpy).toBeCalledWith(7);
-        expect(effect.save).toBeCalledWith(7);
+        expect(saveSpy).toBeCalledWith(7);
         await sleep(0);
         expect(setCountSpy).toBeCalledWith(3);
     });
