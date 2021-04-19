b52cda8c5fea36ed48504c38f9670916e8837446
harunou <d.harunou@gmail.com>
2021-04-19 15:10:16 +0200

    Vscode: format tsx on save

--- src/modules/feature/feature.tsx
+++ src/modules/feature/feature.tsx
@@ -1,11 +1,11 @@
-import { FC } from "react"
+import { FC } from 'react';
 
 export interface FeatureProps {
     text: string;
 }
 
-const Feature: FC<FeatureProps>  =({text}) => {
-    return <h1>{text}</h1>
-}
+const Feature: FC<FeatureProps> = ({ text }) => {
+    return <h1>{text}</h1>;
+};
 
-export default Feature
+export default Feature;
