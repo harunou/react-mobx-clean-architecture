4f5549209ef856420ff850a007d37a28f8c350d0
harunou <d.harunou@gmail.com>
2021-05-02 17:17:24 +0200

    Counter: add 1 and save pessimistic test

--- src/modules/counter/counter.spec.tsx
+++ src/modules/counter/counter.spec.tsx
@@ -5,8 +5,12 @@ import { RootStore, RootStoreContext } from '@stores/root/root.store';
 import { httpClient } from '@core/http-client';
 import assert from 'assert';
 import { Count } from './adapter/selectors/count/count.selector';
-import { COUNTER_SAVE_COUNT_ENDPOINT } from '@api/counter.service';
+import {
+    COUNTER_INCREMENT_ENDPOINT,
+    COUNTER_SAVE_COUNT_ENDPOINT
+} from '@api/counter.service';
 import { MultiplyCount } from './adapter/selectors/multiply-count/multiply-count.selector';
+import { sleep } from '@testing-tools/testing-tools.helpers';
 
 describe(`${Counter.name}`, () => {
     const count = 3;
@@ -71,7 +75,7 @@ describe(`${Counter.name}`, () => {
         expect(MultiplyCount.runs).toEqual(4);
     });
 
-    it('increments store, saves optimistic and renders result', () => {
+    it('increments store, saves optimistic and renders result', async () => {
         const sut = (
             <RootStoreContext.Provider value={rootStore}>
                 <Counter />
@@ -91,10 +95,57 @@ describe(`${Counter.name}`, () => {
         assert(selectMultiplyCount);
 
         fireEvent.click(button);
-        httpClient.match(COUNTER_SAVE_COUNT_ENDPOINT).resolve(count + 1);
+        let pendingRequest = httpClient.match<number, { count: number }>(
+            COUNTER_SAVE_COUNT_ENDPOINT
+        );
+        pendingRequest.resolve(pendingRequest.params.count);
+        await sleep();
 
         fireEvent.click(button);
-        httpClient.match(COUNTER_SAVE_COUNT_ENDPOINT).resolve(count + 2);
+        pendingRequest = httpClient.match<number, { count: number }>(
+            COUNTER_SAVE_COUNT_ENDPOINT
+        );
+        pendingRequest.resolve(pendingRequest.params.count);
+        await sleep();
+
+        expect(selectCount).toHaveTextContent(`${count + 2}`);
+        expect(selectMultiplyCount).toHaveTextContent(`${(count + 2) * 10}`);
+        expect(Count.runs).toEqual(3);
+        expect(MultiplyCount.runs).toEqual(3);
+    });
+
+    it('increments store, saves pessimistic and renders result', async () => {
+        const sut = (
+            <RootStoreContext.Provider value={rootStore}>
+                <Counter />
+            </RootStoreContext.Provider>
+        );
+        const { queryByTestId } = render(sut);
+
+        const button = queryByTestId(
+            counterTestIds.add_1_andSavePessimisticButton
+        );
+        assert(button);
+        const selectCount = queryByTestId(counterTestIds.selectCount);
+        assert(selectCount);
+        const selectMultiplyCount = queryByTestId(
+            counterTestIds.selectMultiplyCount
+        );
+        assert(selectMultiplyCount);
+
+        fireEvent.click(button);
+        let pendingRequest = httpClient.match<number, { increment: number }>(
+            COUNTER_INCREMENT_ENDPOINT
+        );
+        pendingRequest.resolve(count + pendingRequest.params.increment);
+        await sleep();
+
+        fireEvent.click(button);
+        pendingRequest = httpClient.match<number, { increment: number }>(
+            COUNTER_INCREMENT_ENDPOINT
+        );
+        pendingRequest.resolve(count + 1 + pendingRequest.params.increment);
+        await sleep();
 
         expect(selectCount).toHaveTextContent(`${count + 2}`);
         expect(selectMultiplyCount).toHaveTextContent(`${(count + 2) * 10}`);
