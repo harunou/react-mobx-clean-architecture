77751b9d765015000db921a5122fa074ebfa07db
harunou <d.harunou@gmail.com>
2021-05-13 18:50:56 +0200

    Jest: tests-setup.js->jest.setup.js

--- jest.config.js
+++ jest.config.js
@@ -133,7 +133,7 @@ module.exports = {
 
     // A list of paths to modules that run some code to configure or set up the testing framework before each test
     // setupFilesAfterEnv: [],
-    setupFilesAfterEnv: ['<rootDir>/src/tests-setup.js'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
     // The number of seconds after which a test is considered as slow and reported as such in the results.
     // slowTestThreshold: 5,
