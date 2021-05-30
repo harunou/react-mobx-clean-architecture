58db23b731a545b1803f3251aa61eb444d1f8ebf
harunou <d.harunou@gmail.com>
2021-05-30 12:31:54 +0200

    Project: add DI container

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,7 @@ const config = {
         ['@babel/preset-react', { runtime: 'automatic' }]
     ],
     plugins: [
+        'babel-plugin-transform-typescript-metadata',
         ['@babel/plugin-proposal-decorators', { legacy: true }],
         ['@babel/plugin-proposal-class-properties', { loose: false }]
     ]
