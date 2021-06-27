4a643ce9f2cbe667b29af80dffbdd2f5a2ffb569
harunou <d.harunou@gmail.com>
2021-06-27 12:46:59 +0200

    Counter: use abstract classes to refer store

--- src/stores/domain/counter/counter.spec.ts
+++ src/stores/domain/counter/counter.spec.ts
@@ -2,7 +2,7 @@ import { Selector } from '@stores/helpers/store.types';
 import { container, injectable } from 'tsyringe';
 import { constructor } from 'tsyringe/dist/typings/types';
 import { CounterStore } from './counter.store';
-import { CounterState } from './counter.types';
+import { CounterModel, CounterState } from './counter.types';
 
 describe(`${CounterStore.name}`, () => {
     let initial: number;
@@ -33,10 +33,12 @@ describe(`${CounterStore.name}`, () => {
     it('can be injected via abstract classes', () => {
         const initial = 0;
         container.registerSingleton(CounterStore);
-        container.registerType(
-            CounterState as constructor<CounterState>,
-            CounterStore
-        );
+        container.register(CounterState as constructor<CounterState>, {
+            useToken: CounterStore
+        });
+        container.register(CounterModel as constructor<CounterModel>, {
+            useToken: CounterStore
+        });
         @injectable()
         class CountSelector implements Selector {
             constructor(private counterStore: CounterStore) {}
@@ -44,8 +46,15 @@ describe(`${CounterStore.name}`, () => {
                 return this.counterStore.count$;
             }
         }
-        container.registerSingleton(CountSelector);
+        const counterState = container.resolve(
+            CounterState as constructor<CounterState>
+        );
+        const counterModel = container.resolve(
+            CounterModel as constructor<CounterModel>
+        );
         const countSelector = container.resolve(CountSelector);
+
+        expect(counterState === counterModel).toEqual(true);
         expect(countSelector.result).toEqual(initial);
     });
 });
